d9f2377f143e33051981d48206c7850484ed1fc6
grebeshkoff <grebeshkoff@gmail.com>
2015-05-30 12:06:49 +0300

    init

---
+++ CoonDiary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoonDiary.Startup))]
+namespace CoonDiary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
